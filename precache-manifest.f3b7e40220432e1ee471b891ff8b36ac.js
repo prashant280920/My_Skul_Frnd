1b465c93bedc3fe3c450d109757306fe36b8ece0
prashant280920 <jayantmehta8447442229@gmail.com>
2020-05-22 01:09:11 +0530

    Updates

---
+++ precache-manifest.f3b7e40220432e1ee471b891ff8b36ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f00d86dd2f22efeb033de43034590c",
+    "url": "/My_Skul_Frnd/index.html"
+  },
+  {
+    "revision": "679e8efc8e1d655c8bc7",
+    "url": "/My_Skul_Frnd/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4f3c53c0c04dd9f76d8e",
+    "url": "/My_Skul_Frnd/static/css/main.118664fb.chunk.css"
+  },
+  {
+    "revision": "679e8efc8e1d655c8bc7",
+    "url": "/My_Skul_Frnd/static/js/2.5a84fc28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_Skul_Frnd/static/js/2.5a84fc28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3c53c0c04dd9f76d8e",
+    "url": "/My_Skul_Frnd/static/js/main.8431eb80.chunk.js"
+  },
+  {
+    "revision": "58a2c6b45b0a13607bc4",
+    "url": "/My_Skul_Frnd/static/js/runtime-main.7a476e37.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/My_Skul_Frnd/static/media/SEGA.12a3db8b.woff"
+  }
+]);
