bea161bf2f5f0bd27c4a8fdb0bc6774b61371536
prashant280920 <jayantmehta8447442229@gmail.com>
2020-05-27 17:19:16 +0530

    added a margin

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SearchBox = ({searchchange}) =>{
 	return (
-		<div className="pa2">
+		<div className="pa2" style={{'marginTop':"-20px"}}>
 		<input className="tc pa3 ba b--green br2 bg-lightest-blue" style={{color:"black"}}
 		 type='search'
 		 placeholder='serach robots'
